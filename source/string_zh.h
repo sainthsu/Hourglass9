750e386202aaad9ca75afab64b9a532a445fba76
steve <saint@aliyun.com>
2016-12-05 18:46:19 +0800

    add cn file

---
+++ source/string_zh.h
@@ -0,0 +1,33 @@
+#pragma once
+
+const static char* STR_MAINMENU_1 = "系统分区备份/还原";
+const statuc char* STR_MAINMENU_2 = "虚拟分区备份/还原";
+const statuc char* STR_MAINMENU_3 = "游戏卡带导出";
+const statuc char* STR_MAINMENU_4 = "杂项";
+const statuc char* STR_MAINMENU_5 = "验证NAND导出";
+
+const statuc char* STR_SYSNAND_OPTIONS = "系统分区备份/还原选项";
+const statuc char* STR_SYSNAND_OPTIONS_1 = "系统分区备份";
+const statuc char* STR_SYSNAND_OPTIONS_2 = "系统分区还原(保留a9lh)";
+const statuc char* STR_SYSNAND_OPTIONS_3 = "安全使用须知导出";
+const statuc char* STR_SYSNAND_OPTIONS_4 = "安全使用须知注入";
+
+const statuc char* STR_EMUNAND_OPTIONS = "虚拟分区备份/还原选项";
+const statuc char* STR_EMUNAND_OPTIONS_1 = "虚拟分区备份";
+const statuc char* STR_EMUNAND_OPTIONS_2 = "虚拟分区还原";
+
+const statuc char* STR_GAMECART_OPTIONS = "游戏卡带导出选项";
+const statuc char* STR_GAMECART_OPTIONS_1 = "卡带导出(完全)";
+const statuc char* STR_GAMECART_OPTIONS_2 = "卡带导出(精简)";
+const statuc char* STR_GAMECART_OPTIONS = "导出解密卡带(完全)";
+const statuc char* STR_GAMECART_OPTIONS = "导出解密卡带(精简)";
+const statuc char* STR_GAMECART_OPTIONS = "卡带导出成CIA";
+const statuc char* STR_GAMECART_OPTIONS = "导出卡带私有头部";
+
+const statuc char* STR_MISC_OPTIONS = "杂项选项";
+const statuc char* STR_MISC_OPTIONS_1 = "系统";
+const statuc char* STR_MISC_OPTIONS_2 = "虚拟";
+const statuc char* STR_MISC_OPTIONS_3 = "GBA VC存档导出";
+const statuc char* STR_MISC_OPTIONS_4 = "GBA VC存档注入";
+const statuc char* STR_MISC_OPTIONS_5 = "生成NCCH";
+const statuc char* STR_MISC_OPTIONS_6 = "系统信息";
